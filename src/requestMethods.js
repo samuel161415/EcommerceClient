08a77799d91568f13d1ffac0774be685003c1a4f
Air <samuelnegalign1@gmail.com>
2022-12-01 14:50:10 +0300

    changing title

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -8,7 +8,7 @@ const currentUser = user && JSON.parse(user).currentUser;
 console.log("currentUser",currentUser)
 const TOKEN = currentUser?.accessToken;
 
-console.log("tokens",token)
+//console.log("tokens",token)
 //const token=JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser.accessToken;
 //console.log("token",TOKEN)
 //const TOKEN="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzMjMyMGM2MDRmYTU4ZmY0NDMwNzliNCIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY2OTcyMjU1MCwiZXhwIjoxNjcwNTg2NTUwfQ.oyQ38MNHk_s1pdanChLsLv-fcWwt8Z4dgLMgHvIlwZg"
