5a8eb64dd1bab7d297b24e9cd34706c07c4e8dce
Air <samuelnegalign1@gmail.com>
2022-12-01 14:20:22 +0300

    correcting token

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -6,12 +6,12 @@ const user = JSON.parse(localStorage.getItem("persist:root"))?.user;
 console.log("user",user)
 const currentUser = user && JSON.parse(user).currentUser;
 console.log("currentUser",currentUser)
-const token = currentUser?.accessToken;
+const TOKEN = currentUser?.accessToken;
 
 console.log("tokens",token)
 //const token=JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser.accessToken;
 //console.log("token",TOKEN)
-const TOKEN="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzMjMyMGM2MDRmYTU4ZmY0NDMwNzliNCIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY2OTcyMjU1MCwiZXhwIjoxNjcwNTg2NTUwfQ.oyQ38MNHk_s1pdanChLsLv-fcWwt8Z4dgLMgHvIlwZg"
+//const TOKEN="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzMjMyMGM2MDRmYTU4ZmY0NDMwNzliNCIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY2OTcyMjU1MCwiZXhwIjoxNjcwNTg2NTUwfQ.oyQ38MNHk_s1pdanChLsLv-fcWwt8Z4dgLMgHvIlwZg"
 
 
 export const publicRequest= axios.create({
