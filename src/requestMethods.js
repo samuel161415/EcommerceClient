6ba788a9580f4ad5ef86b22089e366a06ef278d1
Air <samuelnegalign1@gmail.com>
2022-12-01 13:43:32 +0300

    checking storage

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const BASE_URL ='https://sami-ecommerce.herokuapp.com/api'
-//const TOKEN=JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser.accessToken;
+console.log("token",JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser)
+//const token=JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser.accessToken;
 //console.log("token",TOKEN)
 const TOKEN="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzMjMyMGM2MDRmYTU4ZmY0NDMwNzliNCIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY2OTcyMjU1MCwiZXhwIjoxNjcwNTg2NTUwfQ.oyQ38MNHk_s1pdanChLsLv-fcWwt8Z4dgLMgHvIlwZg"
 
