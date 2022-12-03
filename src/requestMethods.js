62b97211adc2060d77387dd1f30df89706a08339
Air <samuelnegalign1@gmail.com>
2022-12-03 11:16:56 +0300

    modifying url

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 //const BASE_URL ='https://sami-ecommerce.herokuapp.com/api'
-const BASE_URL = "https://sami-ecommerce.herokuapp.com/api/";
+const BASE_URL = "https://gebeya.onrender.com/api";
 //const TOKEN=JSON.parse(JSON.parse(localStorage.getItem("persist:root"))?.user).currentUser.accessToken;
 //console.log("token",TOKEN)
 //const TOKEN="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzMjMyMGM2MDRmYTU4ZmY0NDMwNzliNCIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY2OTcyMjU1MCwiZXhwIjoxNjcwNTg2NTUwfQ.oyQ38MNHk_s1pdanChLsLv-fcWwt8Z4dgLMgHvIlwZg"
@@ -12,11 +12,11 @@ export const publicRequest= axios.create({
 })
 
 //const TOKEN=
-const TOKEN=()=>{
-   // console.log("called")
-   // console.log("USER",JSON.parse(localStorage.getItem("persist:root"))?.user)
-    return localStorage.getItem("persist:root")&& JSON.parse(JSON.parse(localStorage.getItem("persist:root"))?.user).currentUser?JSON.parse(JSON.parse(localStorage.getItem("persist:root"))?.user).currentUser.accessToken:null
-} 
+// const TOKEN=()=>{
+//    // console.log("called")
+//    // console.log("USER",JSON.parse(localStorage.getItem("persist:root"))?.user)
+//     return localStorage.getItem("persist:root")&& JSON.parse(JSON.parse(localStorage.getItem("persist:root"))?.user).currentUser?JSON.parse(JSON.parse(localStorage.getItem("persist:root"))?.user).currentUser.accessToken:null
+// } 
 export const userRequest= axios.create({
     baseURL:BASE_URL,
     headers: { token: `Bearer ${localStorage.getItem("persist:root")&& JSON.parse(JSON.parse(localStorage.getItem("persist:root"))?.user).currentUser?JSON.parse(JSON.parse(localStorage.getItem("persist:root"))?.user).currentUser.accessToken:null}` }
